9f5ec3d17c236a74ba8645d897e7c33a9a006910
Shreya Sunal <shreyasunal@gmail.com>
2026-09-07 10:15:34 +0530

    Time: 46 ms (66.69%), Space: 29.2 MB (46.06%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>> ans;
         int n = nums.size();
 
-        for(int i = 0; i<=n-2; i++){
+        for(int i = 0; i<=n-1; i++){
             if(i > 0 && nums[i] == nums[i-1]){
                 continue;
             }
