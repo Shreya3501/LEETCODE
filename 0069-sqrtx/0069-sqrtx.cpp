7448fb4c983e47c00c3be51d03ab346145f94a9f
Shreya Sunal <shreyasunal@gmail.com>
2026-09-10 01:35:19 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.33%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    long long int binarySearch(int x){
+
+        int left = 0 ;
+        int right = x;
+        long long int ans = -1;
+
+        while(left <= right){
+
+            long long int mid = left + (right - left)/2;
+            long long int square = mid * mid;
+
+            if(square == x){
+
+                return mid ; 
+
+            }
+            else if(square < x){
+                
+                ans = mid;
+                left = mid + 1;
+
+            }
+            else{
+
+                right = mid - 1;
+
+            }
+        }
+        return ans;
+    }  
+    
+    int mySqrt(int x) {
+
+        return binarySearch(x);
+        
+    }
+};
